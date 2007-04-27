f793cd5ac9830b617a9c8fac349549a5c549749a
Deniz Yuret <denizyuret@gmail.com>
2007-04-27 05:13:51 +0000

    Fixed kn model.

--- model.pl
+++ model.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-warn q{$Id: model.pl,v 2.3 2007/04/15 14:05:29 dyuret Exp dyuret $ } ."\n";
+warn q{$Id: model.pl,v 2.4 2007/04/16 11:31:50 dyuret Exp dyuret $ } ."\n";
 
 use strict;
 use Getopt::Long;
@@ -7,11 +7,13 @@ use PDL;
 use PDL::Opt::Simplex;
 require 'gngram.pl';
 require 'fileio.pl';
+require 'dhc.pl';
 
 # Command line options:
 
 my $verbose = 0;
 my $optimize = 0;
+my $dhc = 0;
 my $patterns;			# output patterns
 my $cachefile;
 my $ngram = 5;
@@ -23,11 +25,12 @@ my @A = (undef, undef, 6.3712181,  6.2403967, 6.2855943,  5.375136); # 8.0605878
 my @B = (undef, undef, 0.00,       0.00,      2.4973338,  2.457501); 
 my @C = (undef, undef, 0.12244049, 0.4886369, 0.74636033, 0.83561995); # 8.22092294839358
 my @D = (undef, undef, 6.7131229,  5.9414447, 6.5528203,  5.7060572); # 8.06083590891475
-my @KN = (undef, 0, 0, 0, 0, 0);
+my @KN = (undef, undef, 0, 0, 0, 0);
 
 GetOptions('cache=s' => \$cachefile,
            'verbose' => \$verbose,
            'optimize' => \$optimize,
+	   'dhc' => \$dhc,
 	   'patterns' => \$patterns,
            'random' => \$random,
            'zeroes' => \$zeroes,
@@ -49,7 +52,6 @@ GetOptions('cache=s' => \$cachefile,
  	   'd3=f' => \$D[3],
  	   'd4=f' => \$D[4],
  	   'd5=f' => \$D[5],
-	   'kn1=f' => \$KN[1],
 	   'kn2=f' => \$KN[2],
 	   'kn3=f' => \$KN[3],
 	   'kn4=f' => \$KN[4],
@@ -67,8 +69,10 @@ my $corpus = read_corpus();
 warn "corpus=" . scalar(@$corpus) . " sentences\n";
 my $nline = 0;
 my $nscore = 0;
+my $debug = 0;
 my $infinity = 1E9;
 my $epsilon = 1E-6;
+my $MINNGRAM = 40;
 my %myngram;
 $optimize ? optimize() : ngram();
 
@@ -78,13 +82,19 @@ sub optimize {
     die "Choose ngram >= 2 for optimization"
 	unless $ngram >= 2;
     my $init = &{$init_fn{$smoothing}}();
+#    warn "init=$init\n";
     warn "initial score:\n";
     my $initscore = &{$score_fn{$smoothing}}($init);
     my $initsize = 1.0;
     my $minsize = 1E-4;
     my $maxiter = 1E6;
-    my ($optimum, $ssize) = simplex($init, $initsize, $minsize,
-				    $maxiter, \&score, \&display);
+    if ($dhc) {
+	my ($optimum, $final) = dhc(\&score, $init);
+	warn "$optimum <= $final\n";
+    } else {
+	my ($optimum, $ssize) = simplex($init, $initsize, $minsize,
+					$maxiter, \&score, \&display);
+    }
 }
 
 sub ngram {
@@ -94,7 +104,7 @@ sub ngram {
 	$nbits += $b; $nword += $w;
     }
     my $avgbits = $nbits / $nword;
-    print "$nword $avgbits\n"
+    warn "$nword $avgbits\n"
 	if not $optimize;
     return $avgbits;
 }
@@ -137,16 +147,18 @@ sub init_baseline {
 
 sub init_kn {
     my $init;
-    my $ndims = $ngram;
+    my $ndims = $ngram - 1;
     $init = $zeroes ? zeroes($ndims) 
 	: $random ? log(1/random($ndims)-1)
-	: pdl(@KN[1 .. $ngram]);
+	: pdl(@KN[2 .. $ngram]);
     return $init;
 }
 
 sub score {
     my $x = shift;
+#    warn "score($x)\n";
     my ($ndims, $npoints) = $x->dims;
+#    warn "ndims=$ndims npoints=$npoints\n";
     if (defined $npoints) {
 	my @score;
 	for (my $i = 0; $i < $npoints; $i++) {
@@ -196,8 +208,8 @@ sub score_baseline {
 sub score_kn {			
     my $x = shift;
     $nscore++;
-    for (my $i = 1; $i <= $ngram; $i++) {
-	$KN[$i] = $x->at($i - 1);
+    for (my $i = 2; $i <= $ngram; $i++) {
+	$KN[$i] = $x->at($i - 2);
     }
     my $bits = ngram();
     warn "score[$nscore]: $bits $x\n";
@@ -227,13 +239,17 @@ sub process_sentence {
     $nline++;
     my $nword;
     my $nbits;
+    if (!$patterns) {
+	for (my $i = 1; $i < $#{$s}; $i++) {
+	    if (myngram($s->[$i]) == 0) { 
+		warn "Warning[$nline]: [$s->[$i]] unknown, skipping sentence.\n"
+		    if not $optimize;
+		return (0, 0);
+	    }
+	}
+    }
     for (my $i = 1; $i < $#{$s}; $i++) {
 	$nword++;
-	if (myngram($s->[$i]) == 0) { 
-	    warn "Warning[$nline]: [$s->[$i]] unknown, skipping sentence.\n"
-		if not $optimize;
-	    return (0, 0);
-	}
 	my $b = bits($s, $i, $ngram);
 	$nbits += $b;
 	if ($verbose) {
@@ -352,29 +368,68 @@ sub bits {
 
 sub kn {
     my ($s, $i, $n) = @_;
-    my ($nxy, $x, $nx, $n1x, $kn, $D);
+    my ($nxy, $x, $nx_, $n1x_, $kn, $D);
     $n = 5 if not defined $n;
     if ($n < 0) {
 	die "Bad n [$n]";
     } elsif ($n == 0) {
-	return 1/myngram('_');
+
+ 	# BUG: no need for 0 order model, if the word is known, this
+ 	# is exactly equivalent to k/N.  If the word is not known it
+ 	# is cheating.
+	
+ 	# return 1/myngram('_');
+	die "Bad n [$n]";
+    } elsif ($n == 1) {
+	my $g = myngram($s->[$i]);
+
+	# BUG: Any word below 200 count is excluded from the google
+	# data.  We will give such words a count of 100.  Note that
+	# this may happen because of tokenization errors, and it is
+	# not probabilistically sound.
+
+	# die "Unknown word [$s->[$i]]" if $g == 0;
+	$g = 100 if $g == 0;
+
+	return $g / $GTotal;
+	
     } elsif ($n > 5) {
 	return kn($s, $i, 5);
     } elsif ($n > $i + 1) {
 	return kn($s, $i, $i+1);
     }    
     $x = ($n > 1) ? join(' ', @{$s}[($i-$n+1) .. ($i-1)]).' ' : '';
-    $nx = myngram($x . ':?:');
-    if ($nx == 0) {
+    $nx_ = myngram($x . ':?:');
+    warn("kn($s->[$i],$i,$n): nx_ = $nx_\n") if $debug;
+    if ($nx_ == 0) {
 	return kn($s, $i, $n-1);
     } elsif (" $x" =~ / [;!?.] /) { # bad context
 	return kn($s, $i, $n-1);
     }	
-    $n1x = myngram($x . '_');
+    $n1x_ = myngram($x . '_');
+    warn("kn($s->[$i],$i,$n): n1x_ = $n1x_\n") if $debug;
     $nxy = myngram($x . $s->[$i]);
-    $D = 1/(1+exp(-$KN[$n]));
+    warn("kn($s->[$i],$i,$n): nxy = $nxy\n") if $debug;
+    # $D = 1/(1+exp(-$KN[$n]));
+    $D = $MINNGRAM/(1+exp(-$KN[$n]));
+    warn("kn($s->[$i],$i,$n): D = $D\n") if $debug;
     $nxy -= $D if $nxy > 0;
-    $kn = $nxy / $nx + ($n1x * $D / $nx) * kn0($s, $i, $n-1);
+    #$kn = $nxy / $nx_ + ($n1x_ * $D / $nx_) * kn0($s, $i, $n-1);
+
+    # new formula:
+    my ($nx, $mc);
+    if ($n == 1) {
+	$nx = $nx_;
+	$mc = 0;
+    } else {
+	$nx = myngram($x);
+	$mc = $nx - $nx_;
+    }
+    warn("kn($s->[$i],$i,$n): x = [$x] nx = $nx\n") if $debug;
+    warn("kn($s->[$i],$i,$n): mc = $mc\n") if $debug;
+    $kn = $nxy / $nx + (($mc + $n1x_ * $D) / $nx) * kn0($s, $i, $n-1);
+
+    warn("kn($s->[$i],$i,$n): kn = $kn\n") if $debug;
     if ($patterns) { 
 	# if outputting patterns do the lower order models as well
 	kn($s, $i, $n-1); 
@@ -382,22 +437,47 @@ sub kn {
     return $kn;
 }
 
+# BUG: The n1 counts are grossly underestimated whereas the n counts
+# are exact.  This especially effects the second term in kn which is a
+# n1/n term.  Must derive corrected n1 counts from the missing count.
+
 sub kn0 {
     my ($s, $i, $n) = @_;
     my ($x, $n1_xy, $n1_x_, $n1x_, $D, $kn0);
     if ($n < 0) {
 	die "Bad n [$n]";
     } elsif ($n == 0) {
-	return 1/myngram('_');
+	# return 1/myngram('_');
+	die "Bad n [$n]";
+    } elsif ($n == 1) {
+	my $n1_y = myngram('_ ' . $s->[$i]);
+	if ($n1_y == 0) {
+	    # warn "n1_y($s->[$i]) == 0";
+	    # BUG: verify that this is exactly what would happen if we smoothed with a 0-order 1/V model
+	    $n1_y = 1;
+	}
+	my $n1__ = myngram('_ _');
+	return $n1_y / $n1__;
     }
     $x = ($n > 1) ? join(' ', @{$s}[($i-$n+1) .. ($i-1)]).' ' : '';
     $n1_x_ = myngram('_ ' . $x . '_');
+    warn("kn0($s->[$i],$i,$n): n1_x_ = $n1_x_\n") if $debug;
     die "KN0 zero denominator [$x]" if $n1_x_ == 0;
     $n1x_ = myngram($x . '_');
+    warn("kn0($s->[$i],$i,$n): n1x_ = $n1x_\n") if $debug;
     $n1_xy = myngram('_ ' . $x . $s->[$i]);
+    warn("kn0($s->[$i],$i,$n): n1_xy = $n1_xy\n") if $debug;
     $D = 1/(1+exp(-$KN[$n]));
+    warn("kn0($s->[$i],$i,$n): D = $D\n") if $debug;
     $n1_xy -= $D if $n1_xy > 0;
     $kn0 = $n1_xy / $n1_x_ + ($n1x_ * $D / $n1_x_) * kn0($s, $i, $n-1);
+
+    # new formula: does not really work
+#     my $n1x = myngram($x);
+#     my $mc = $n1x - $n1_x_;
+#     $kn0 = $n1_xy / $n1x + (($mc + $n1x_ * $D) / $n1x) * kn0($s, $i, $n-1);
+
+    warn("kn0($s->[$i],$i,$n): kn0 = $kn0\n") if $debug;
     return $kn0;
 }
 
@@ -410,6 +490,15 @@ sub myngram {
 	}
 	return 1;
     } else {
+
+	# BUG: temporary fix
+	$str =~ s/^\s+//;
+	$str =~ s/\s+$//;
+	if ($str =~ /^<S>$/) {
+	    return 95119665584;
+	}
+	# End of temporary fix
+
 	return gngram($str);
     }
 }
